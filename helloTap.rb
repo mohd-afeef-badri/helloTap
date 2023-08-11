77914903ba7914ca1f64bcf3dc7bcf61260a7f31
Afeef <afeef.badri@gmail.com>
2023-08-11 10:41:39 +0200

    correction SHA256 mismatch

--- helloTap.rb
+++ helloTap.rb
@@ -2,7 +2,7 @@ class Hellotap < Formula
   desc "Simple program that prints 'Hello Tap!'"
   homepage "https://github.com/mohd-afeef-badri/homebrew-hellotap"
   url "https://codeload.github.com/mohd-afeef-badri/homebrew-hellotap/tar.gz/refs/tags/v1.0.0"
-  sha256 "eef0b722380fa3e728cc56480d3a97842d43bbdcb4b0ef2a068769653f66e955"
+  sha256 "c71c0dad7ae95dead18673bc819012af1ceb5d894bfc4358d31dfde7d313e206"
 
   def install
     bin.install "helloTap"
