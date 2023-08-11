719cc63fffe8e9073522c2768d5ff9d6c14b20af
Afeef <afeef.badri@gmail.com>
2023-08-11 10:31:14 +0200

    new message

--- helloTap.rb
+++ helloTap.rb
@@ -1,7 +1,7 @@
 class Hellotap < Formula
   desc "Simple program that prints 'Hello Tap!'"
-  homepage "https://github.com/mab/helloTap"
-  url "https://codeload.github.com/mohd-afeef-badri/helloTap/tar.gz/refs/tags/v1.0.0"
+  homepage "https://github.com/mohd-afeef-badri/homebrew-hellotap"
+  url "https://codeload.github.com/mohd-afeef-badri/homebrew-hellotap/tar.gz/refs/tags/v1.0.0"
   sha256 "eef0b722380fa3e728cc56480d3a97842d43bbdcb4b0ef2a068769653f66e955"
 
   def install
