1fa5c9174983a5d49c14eecdadeee1898cdbb169
Afeef <afeef.badri@gmail.com>
2023-08-11 10:50:24 +0200

    new version SHA

--- helloTap.rb
+++ helloTap.rb
@@ -1,8 +1,8 @@
 class Hellotap < Formula
   desc "Simple program that prints 'Hello Tap!'"
   homepage "https://github.com/mohd-afeef-badri/homebrew-hellotap"
-  url "https://codeload.github.com/mohd-afeef-badri/homebrew-hellotap/tar.gz/refs/tags/v1.0.0"
-  sha256 "c71c0dad7ae95dead18673bc819012af1ceb5d894bfc4358d31dfde7d313e206"
+  url "https://codeload.github.com/mohd-afeef-badri/homebrew-hellotap/tar.gz/refs/tags/v1.0.1"
+  sha256 "8f3fc2bc531a91210b97e8e16e1eada38b26c60bb91bc7cbaa66b554f71649b4"
 
   def install
     bin.install "helloTap"
