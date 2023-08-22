30c8ee6da2a971a2afb8735e1fd8d77898b6a1c2
Mohd Afeef Badri <52162083+mohd-afeef-badri@users.noreply.github.com>
2023-08-22 15:09:18 +0200

    correct sha256

--- helloBrew.rb
+++ helloBrew.rb
@@ -2,7 +2,7 @@ class Hellobrew < Formula
   desc "Simple program that prints 'Hello Brew!'"
   homepage "https://github.com/mohd-afeef-badri/homebrew-hellobrew"
   url "https://codeload.github.com/mohd-afeef-badri/homebrew-hellobrew/tar.gz/refs/tags/v1.0.2"
-  sha256 "baa128701816989f65106f6efb4f4ccd44bbe65c30e989310a7f81070576942b"
+  sha256 "1e197c0af42418ad0bdaec26bf56a253d1cd946b881b7ea5f646d172f960fbd1"
   def install
     bin.install "helloTap"
   end
